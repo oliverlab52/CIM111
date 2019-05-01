fc0ed37a56a1d17e497baea8da16a8c1c678257b
oliverlab52 <46943302+oliverlab52@users.noreply.github.com>
2019-05-01 00:29:54 -0400

    Add files via upload

---
+++ a5/Guitar Chords How-To/chords.js
@@ -0,0 +1,27 @@
+var x = document.getElementById("Cchord"); 
+var y = document.getElementById("Gchord"); 
+var w = document.getElementById("Dchord");
+
+function playCchord() { 
+  x.play(); 
+} 
+
+function pauseCchord() { 
+  x.pause(); 
+} 
+
+function playGchord() { 
+  y.play(); 
+} 
+
+function pauseGchord() {
+  y.pause();
+}
+
+function playDchord() {
+  z.play();
+}
+
+function pauseDchord() {
+  z.pause();
+}
